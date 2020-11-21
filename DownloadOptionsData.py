bd3381b4ffa0c882b7258af6cc1fc96daebf6ff2
Robin Lan <rolan@microsoft.com>
2020-11-21 16:17:56 +0800

    Change path to Linux

--- DownloadOptionsData.py
+++ DownloadOptionsData.py
@@ -7,8 +7,8 @@ import multiprocessing
 
 DataRoot = "/datadrive/data"
 LogRoot = "/datadrive//log.txt"
-Holidays = "/datadrive/github/holidays.txt"
-SymbolRoot = "/datadrive/github"
+Holidays = "/datadrive/github/OptionStragtedy/holidays.txt"
+SymbolRoot = "/datadrive/github/OptionStragtedy"
 
 def DownloadAllData(symbol, date):
     if date.dayofweek == 5 or date.dayofweek == 6:
@@ -21,34 +21,34 @@ def DownloadAllData(symbol, date):
     s = yf.Ticker(symbol)
 
     #确保这个目录存在
-    symbolPath = DataRoot +"\\" +symbol
+    symbolPath = DataRoot +"/" +symbol
     os.makedirs(symbolPath, exist_ok = True)
     logger.debug("Get stock price for " + symbol +" at " + dateStr)
     try:
         todayData = s.history(period="1d", interval="1d", start=dateStr)
-        with open(symbolPath +"\\stock.csv", "a") as stock:
+        with open(symbolPath +"/stock.csv", "a") as stock:
             stock.write(dateStr +",")
             stock.writelines(",".join(map(str, todayData.values[-1]))+ "\n", )
     except Exception as e:
         logger.error("Get history for" + symbol + " get exception:" + str(e))
 
-    datePathStr = symbolPath +"\\" + dateStr
+    datePathStr = symbolPath +"/" + dateStr
     try:
         expirations = s.options
         logger.debug("Get options for " + symbol +" at " + dateStr + ". There are " + len(expirations) +" expirations.")
         os.makedirs(datePathStr, exist_ok = True)
         for exp in expirations:
             chain = s.option_chain(exp)
-            if not os.path.exists(datePathStr +"\\README.txt"):
-                with open(datePathStr +"\\README.txt", "w") as readmef:
+            if not os.path.exists(datePathStr +"/README.txt"):
+                with open(datePathStr +"/README.txt", "w") as readmef:
                     readmef.writelines("CALL COLUMNS:")
                     readmef.writelines(",".join(chain.calls.columns) +"\n")
                     readmef.writelines("PUT COLUMNS:")
                     readmef.writelines(",".join(chain.puts.columns) +"\n")
-            with open(datePathStr +"\\calls"+ exp +".csv", "w") as callf:
+            with open(datePathStr +"/calls"+ exp +".csv", "w") as callf:
                 for call in chain.calls.values:
                     callf.writelines(",".join(map(str, call)) + "\n")
-            with open(datePathStr +"\\puts" + exp +".csv", "w") as putf:
+            with open(datePathStr +"/puts" + exp +".csv", "w") as putf:
                 for put in chain.puts.values:
                     putf.writelines(",".join(map(str, put)) + "\n")
         logger.debug("Got options for " + symbol +" at " + dateStr)
@@ -68,7 +68,7 @@ def GetAllData():
     allSymbols = []
     symbolFiles = ["DJIA.txt", "nasdaq100.txt", "SP500.txt"]
     for sf in symbolFiles:
-        with open(SymbolRoot +"\\" + sf) as df:
+        with open(SymbolRoot +"/" + sf) as df:
             allSymbols.extend(df.read().splitlines())
     allSymbols = list(set(allSymbols))
 
